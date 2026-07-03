c39ba800ec8ff06882db2155d39395a49ac7b786
User <User@Mac-mini.local>
2026-07-03 12:45:43 +0300

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://vdiga.bg",
+  base: process.env.GITHUB_PAGES === "true" ? "/vdiga-site" : "/",
   output: "static",
 });
